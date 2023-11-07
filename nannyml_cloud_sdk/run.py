e0c967fb0580b669ab2db5749847e34a8bcbd87c
Michael Van de Steene <michael@nannyml.com>
2023-11-07 10:12:48 +0100

    Add `Run.trigger` operation

--- nannyml_cloud_sdk/run.py
+++ nannyml_cloud_sdk/run.py
@@ -1,6 +1,10 @@
 import datetime
 from typing import TypedDict
 
+from gql import gql
+
+from .client import get_client
+
 
 RUN_SUMMARY_FRAGMENT = """
     fragment RunSummary on Run {
@@ -13,6 +17,14 @@ RUN_SUMMARY_FRAGMENT = """
     }
 """
 
+_START_RUN = gql("""
+    mutation startRun($modelId: Int!) {
+        start_model_run(modelId: $modelId) {
+            id
+        }
+    }
+""")
+
 
 class RunSummary(TypedDict):
     id: str
@@ -21,3 +33,12 @@ class RunSummary(TypedDict):
     startedAt: datetime.datetime
     completedAt: datetime.datetime
     ranSuccessfully: bool
+
+
+class Run:
+    """Operations for running NannyML model analysis"""
+
+    @classmethod
+    def trigger(cls, model_id: str) -> str:
+        """Trigger analysis of new data for a model"""
+        return get_client().execute(_START_RUN, {"modelId": int(model_id)})["start_model_run"]["id"]
